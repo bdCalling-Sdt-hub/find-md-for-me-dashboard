ad25b449f006285f786b600b52ff02bb65667dce
AbdurRahman4991 <engrabdurrahman4991@gmail.com>
2024-08-22 18:25:01 +0600

    update env file

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: "192.168.10.201",
-    // port: "3000"
     host: "159.65.14.5",
     port: "3001"
   }
