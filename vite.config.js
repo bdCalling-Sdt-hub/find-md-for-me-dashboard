b5bb46020fd804b82534ce1be8cbd7927bd436e1
AbdurRahman4991 <engrabdurrahman4991@gmail.com>
2024-08-31 14:39:24 +0600

    update env fiel

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-  // host: "159.65.14.5",
-  // port: "3001"
+  host: "159.65.14.5",
+  port: "3001"
   }
   // host: "159.65.14.5",
   // port: "3001"
